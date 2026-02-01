305ed13e3498c48fc30f1cb326fb6503e3aa3a5b
Nelson Malbone <nhmalbone@icloud.com>
2026-01-31 20:51:01 -0500

    getting content

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 request = requests.get('https://books.toscrape.com/catalogue/category/books_1/index.html') # website request
-print(request) # confirm a request 200
+# print(request) # confirm a request 200
+
+# getting content from request
+content = request.content
+# print(content) # testing to get content
