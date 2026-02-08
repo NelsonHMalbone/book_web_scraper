dbc219982049171a786d95643752b21c103b03ef
Nelson Malbone <nhmalbone@icloud.com>
2026-02-08 14:08:04 -0500

    gotten the first page of the website scrape for the title of the books to be shown

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-request = requests.get('https://books.toscrape.com/catalogue/category/books_1/index.html') # website request
+url = 'https://books.toscrape.com/catalogue/category/books_1/index.html'
+request = requests.get(url) # website request
 # print(request) # confirm a request 200
 
 # getting content from request
-content = request.content
-# print(content) # testing to get content
+content_soup = BeautifulSoup(request.text, 'html.parser')
+
+# gather price, title, and availability
+
+# elem title
+elems_title = content_soup.select('h3 > a')
+for elem in elems_title:
+    print(elem.text)
