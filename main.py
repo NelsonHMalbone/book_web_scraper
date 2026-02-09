5a0cc1359caf2630d8a8936fcb6daf38bbb11307
Nelson Malbone <nhmalbone@icloud.com>
2026-02-09 13:34:02 -0500

    back to the beginning

--- main.py
+++ main.py
@@ -5,12 +5,7 @@ url = 'https://books.toscrape.com/catalogue/category/books_1/index.html'
 request = requests.get(url) # website request
 # print(request) # confirm a request 200
 
+
 # getting content from request
 content_soup = BeautifulSoup(request.text, 'html.parser')
-
-# gather price, title, and availability
-
-# elem title
-elems_title = content_soup.select('h3 > a')
-for elem in elems_title:
-    print(elem.text)
+print(content_soup)
