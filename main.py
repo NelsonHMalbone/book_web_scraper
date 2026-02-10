106e5e0a73ef6322f9228cc3bbf7378f330c2627
Nelson Malbone <nhmalbone@icloud.com>
2026-02-10 15:23:55 -0500

    got the titles but it also showing none in the print out

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ request = requests.get(url) # website request
 # getting content from request
 content_soup = BeautifulSoup(request.text, 'html.parser')
 print(content_soup)
+
+for titles in content_soup.find_all('a'):
+    gather_titles = titles.get('title')
+    print(gather_titles)
